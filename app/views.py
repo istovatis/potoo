e3ab254757581a52e0e75c353c0d176213ebefcd
istovatis <istovatis@gmail.com>
2016-02-16 22:50:54 +0200

    Show pots based on time created. Show user name instead of his id.
    Signed-off-by: istovatis <istovatis@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
 import datetime
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .forms import NewPot
-
-
 from .models import Pot, User
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -21,19 +19,24 @@ def index(request):
             pot.creator = 'd64400d3-dfa3-4331-a762-efdd547f21bc'
             pot.save()
 
-    latest_pots_list = Pot.objects[:5]
+    latest_pots_list = Pot.objects[:15]
     template = loader.get_template('pots/index.html')
     context = {
-        'latest_pots_list': latest_pots_list,
+        'latest_pots_list': sorted(latest_pots_list, key=lambda n:n.time_created, reverse=True),
     }
     return HttpResponse(template.render(context, request))
 
 def detail(request, id):
     try:
         pot = Pot.objects.get(pot_id=id)
+        context = {
+            'pot': pot,
+            'user': User.objects.get(user_id=pot.creator)
+
+        }
     except Pot.DoesNotExist:
         raise Http404("Oops... Not pot")
-    return render(request, 'pots/detail.html', {'pot': pot})
+    return render(request, 'pots/detail.html', context)
 
 def user_detail(request, id):
     try:
