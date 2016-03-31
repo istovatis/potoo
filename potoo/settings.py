33bf7b4a51debd0c0bd3874f5ea41bfa5f8f1808
istovatis <istovatis@gmail.com>
2016-03-31 23:12:48 +0300

    Update wsgi for heroku
    Signed-off-by: istovatis <istovatis@gmail.com>

--- potoo/settings.py
+++ potoo/settings.py
@@ -100,6 +100,18 @@ DATABASES = {
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 # Password validation
